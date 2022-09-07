cf31eec3b550a265268aefd4b871227cd6ec3daf
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-09-07 10:51:51 +0800

    Adding Temperature and Decibel

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Temperature;
+use App\Models\Decibel;
 
 class APIController extends Controller
 {
     public function temperature($temperature, $lightStatus) {
+        Temperature::create([
+            'temperature' => $temperature,
+            'light_status' => $lightStatus
+        ]);
+
         return response([
             'temperature' => $temperature,
             'lightStatus' => $lightStatus
@@ -14,6 +21,11 @@ class APIController extends Controller
     }
 
     public function sound($decibel, $musicStatus) {
+        Decibel::create([
+            'decibel' => $decibel,
+            'sound_status' => $musicStatus
+        ]);
+
         return response([
             'decibel' => $decibel,
             'musicStatus' => $musicStatus
