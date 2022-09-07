878ed3cba1bdc96c0118c2ff6f604504ad54a63d
Harley Ferrer Lumagui <lumagui.harl0723@gmail.com>
2022-09-07 10:48:37 +0800

    Created Migration and Model for Decibel

---
+++ database/migrations/2022_09_07_024717_create_decibels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decibels', function (Blueprint $table) {
+            $table->id();
+            $table->double('decibel');
+            $table->boolean('sound_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decibels');
+    }
+};
